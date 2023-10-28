2d6ef5551c320164f0b630017848d89275bda3e5
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-10-28 13:02:06 +0700

    no2

---
+++ ArrayNilai03.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ArrayNilai03{
+    public static void main(String[] args){
+        Scanner scan = new Scanner (System.in);
+
+        int [] nilaiAkhir = new int [10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Masukkan nilai akhir ke-"+i+" : ");
+            nilaiAkhir[i] = scan.nextInt();
+
+        }
+        for (int i = 0; i < 10; i++){
+            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        }
+    }
+}
